8263c637021513e6c89a2050f17fc6e009501914
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-12-07 21:01:59 +0200

    Uploaded Successfully

---
+++ plugins/جيت هوب تحميل.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch'
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+let handler = async (m, { args, usedPrefix, command }) => {
+if (!args[0]) throw `*مـــش صـح كــــدا*\n\n   *انـت نــســيـت الࢪابـط اللي هـتحمـله* `
+if (!regex.test(args[0])) throw `${lenguajeGB['smsAvisoFG']()}${mid.smsgit}`
+try {   
+let [_, user, repo] = args[0].match(regex) || []
+repo = repo.replace(/.git$/, '')
+let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+m.reply(`*جــــــاࢪ التـحمـــــيل......*`)
+conn.sendFile(m.chat, url, filename, null, m)
+} catch (e) { 
+await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, m)
+console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
+console.log(e)
+handler.limit = 0 //❌No gastada diamante si el comando falla
+}}
+handler.help = ['gitclone <url>']
+handler.tags = ['downloader']
+handler.command = /gitclone|جيت_كولين|clonarrepo|repoclonar/i
+handler.limit = 2
+handler.level = 3
+handler.register = true
+export default handler
+
+/*let info = `💖 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n💝 *Find out about what's new and remember to have the latest version.*
+  `.trim()
+await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
+['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
+['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,) */
