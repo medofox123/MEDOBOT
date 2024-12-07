20d60ddc995cf97e0b1e9009d9f38334eaf0bbf0
medofox123 <135126620+medofox123@users.noreply.github.com>
2024-12-07 21:03:04 +0200

    Uploaded Successfully

---
+++ plugins/بينج.js
@@ -0,0 +1,107 @@
+import { cpus as _cpus, totalmem, freemem } from 'os'
+import util from 'util'
+import os from 'os'
+import fetch from 'node-fetch'
+import osu from 'node-os-utils'
+import { performance } from 'perf_hooks'
+import { sizeFormatter } from 'human-readable'
+let format = sizeFormatter({
+std: 'JEDEC', // 'SI' (default) | 'IEC' | 'JEDEC'
+decimalPlaces: 2,
+keepTrailingZeroes: false,
+render: (literal, symbol) => `${literal} ${symbol}B`,
+})
+let handler = async (m, { conn, usedPrefix, command, isRowner}) => {
+try {
+let enlace = { contextInfo: { externalAdReply: {title: wm + ' 🐈', body: 'support group' , sourceUrl: accountsgb, thumbnailUrl: await(await fetch(gataMenu)).buffer() }}}
+let enlace2 = { contextInfo: { externalAdReply: { showAdAttribution: true, mediaUrl: yt, mediaType: 'VIDEO', description: '', title: wm, body: '𝗙𝗢𝗫 𝗪𝗛𝗔𝗧"𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 ', thumbnailUrl: await(await fetch(gataMenu)).buffer(), sourceUrl: accountsgb }}}
+let dos = [enlace, enlace2]
+
+let _muptime
+if (process.send) {
+process.send('uptime')
+_muptime = await new Promise(resolve => {
+process.once('message', resolve)
+setTimeout(resolve, 1000)
+}) * 1000 }
+  
+let muptime = clockString(_muptime)
+const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
+const groupsIn = chats.filter(([id]) => id.endsWith('@g.us')) //groups.filter(v => !v.read_only)
+const used = process.memoryUsage()
+const cpus = _cpus().map(cpu => {
+cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+return cpu
+})
+
+const cpu = cpus.reduce((last, cpu, _, { length }) => {
+last.total += cpu.total
+last.speed += cpu.speed / length
+last.times.user += cpu.times.user
+last.times.nice += cpu.times.nice
+last.times.sys += cpu.times.sys
+last.times.idle += cpu.times.idle
+last.times.irq += cpu.times.irq
+return last
+  
+}, {
+speed: 0,
+total: 0,
+times: {
+user: 0,
+nice: 0,
+sys: 0,
+idle: 0,
+irq: 0
+}})
+
+let old = performance.now()
+await conn.reply(m.chat, `*🚀 انــــتـظࢪ جـــاࢪ الـــحـساب*...`, fkontak, {contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: '𝗙𝗢𝗫 𝗪𝗛𝗔𝗧"𝗦𝗔𝗣𝗣 𝗕𝗢𝗧 ', previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb }}})
+//await conn.sendButton(m.chat, wm, `${eg}🚀 𝙋𝙍𝙐𝙀𝘽𝘼 𝘿𝙀 𝙑𝙀𝙇𝙊𝘾𝙄𝘿𝘼𝘿...\n🚀 𝙎𝙋𝙀𝙀𝘿 𝙏𝙀𝙎𝙏...`, null, [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], fkontak, m)  
+
+let neww = performance.now()
+let speed = neww - old
+let caption = `*ســـــࢪعــــه الــــبوت*
+
+🚄 *${Math.round(neww - old)}* ms
+🚄 *${speed}* ms
+
+*${htjava}* زمـن التــشــغــــيل 
+${muptime}
+${readMore}
+*🟢 RAM:* ${format(totalmem() - freemem())} / ${format(totalmem())}
+*🔵 RAM Libre:* ${format(freemem())}
+
+*💻 Plataforma :* \`\`\`${os.platform()}\`\`\`
+*📡 Servidor :* _${os.hostname()}_
+${readMore}
+*NodeJS Uso de memoria : Memory Usage*
+${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
+
+${cpus[0] ? `*Uso total de la CPU*
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+
+*CPU Core(s) Uso (${cpus.length} Core CPU)*
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}`
+await conn.sendFile(m.chat, gataImg, 'pp.jpg', caption, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb }}})
+//conn.sendFile(m.chat, gataImg.getRandom(), 'gata.jpg', caption, fkontak)
+//await conn.sendButton(m.chat, wm, caption, gata.getRandom(), [['𝗠 𝗘 𝗡 𝗨 ☘️', '/menu']], m, dos.getRandom())
+} catch (e) {
+await conn.reply(m.chat, `${fg}*ALGO SALIÓ MAL. ERRORS FOUND.*\n\n\`\`\`REPORTE ESTE COMANDO ${usedPrefix + command} CON EL COMANDO ${usedPrefix}reporte\`\`\``, m)
+console.log(e)
+}}
+handler.help = ['ping', 'speed']
+handler.tags = ['info', 'tools']
+handler.command = /^(بينج|بنج|velocidad|rapidez|velocity)$/i
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [' ' + d, ' *يــــــوم ☀️*\n ', h, ' *ســـــــاعـه 🕐*\n ', m, ' *دقــــيقـــه ⏰*\n ', s, ' *ثـــانـــــيه ⏱️* '].map(v => v.toString().padStart(2, 0)).join('')
+}
